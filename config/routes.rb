30e441ee9ac9040adf51c18a2bc0ef2833dcb30e
amax3002 <alexandremguy@gmil.com>
2016-12-01 16:39:14 -0500

    setup user, session, and tweet controller and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'tweets/new'
+
+  get 'session/new'
+
+  get 'statuses/new'
+
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
